88badf69ac42ce4fa428caa4f45fd9c832ff2eb0
Archita8 <91133125+Archita8@users.noreply.github.com>
2023-10-04 16:41:37 +0530

    Add files via upload

---
+++ websiteCode/farm_mates.js
@@ -0,0 +1,94 @@
+function sensor()
+{
+    var a=document.getElementById("t1").value;
+    if(a=="Barley")
+    {
+        var b=parseInt(document.getElementById("t2").value=450);
+        var c=parseInt(document.getElementById("t3").value=650);
+    }
+    else if(a=="Oats")
+    {
+        var b=parseInt(document.getElementById("t2").value=450);
+        var c=parseInt(document.getElementById("t3").value=650);
+    }
+    else if(a=="Wheat")
+    {
+        var b=parseInt(document.getElementById("t2").value=450);
+        var c=parseInt(document.getElementById("t3").value=650);
+    }
+    else if(a=="Bean")
+    {
+        var b=parseInt(document.getElementById("t2").value=300);
+        var c=parseInt(document.getElementById("t3").value=500);
+    }
+    else if(a=="Cabbge")
+    {
+        var b=parseInt(document.getElementById("t2").value=350);
+        var c=parseInt(document.getElementById("t3").value=500);
+    }
+    else if(a=="Cotton")
+    {
+        var b=parseInt(document.getElementById("t2").value=700);
+        var c=parseInt(document.getElementById("t3").value=1300);
+    }
+    else if(a=="Maize")
+    {
+        var b=parseInt(document.getElementById("t2").value=600);
+        var c=parseInt(document.getElementById("t3").value=800);
+    }
+    else if(a=="Melon")
+    {
+        var b=parseInt(document.getElementById("t2").value=400);
+        var c=parseInt(document.getElementById("t3").value=600);
+    }
+    else if(a=="Onion")
+    {
+        var b=parseInt(document.getElementById("t2").value=300);
+        var c=parseInt(document.getElementById("t3").value=550);
+    }
+    else if(a=="Peanut")
+    {
+        var b=parseInt(document.getElementById("t2").value=550);
+        var c=parseInt(document.getElementById("t3").value=700);
+    }
+    else if(a=="Patato")
+    {
+        var b=parseInt(document.getElementById("t2").value=500);
+        var c=parseInt(document.getElementById("t3").value=700);
+    }
+    else if(a=="Pea")
+    {
+        var b=parseInt(document.getElementById("t2").value=350);
+        var c=parseInt(document.getElementById("t3").value=500);
+    }
+    else if(a=="Pepper")
+    {
+        var b=parseInt(document.getElementById("t2").value=600);
+        var c=parseInt(document.getElementById("t3").value=900);
+    }
+    else if(a=="Rice")
+    {
+        var b=parseInt(document.getElementById("t2").value=450);
+        var c=parseInt(document.getElementById("t3").value=700);
+    }
+    else if(a=="Soybean")
+    {
+        var b=parseInt(document.getElementById("t2").value=450);
+        var c=parseInt(document.getElementById("t3").value=700);
+    }
+    else if(a=="Suggarcane")
+    {
+        var b=parseInt(document.getElementById("t2").value=1550);
+        var c=parseInt(document.getElementById("t3").value=2500);
+    }
+    else if(a=="Sunflower")
+    {
+        var b=parseInt(document.getElementById("t2").value=600);
+        var c=parseInt(document.getElementById("t3").value=1000);
+    }
+    else
+    {
+        var b=parseInt(document.getElementById("t2").value=400);
+        var c=parseInt(document.getElementById("t3").value=800);
+    }
+}
